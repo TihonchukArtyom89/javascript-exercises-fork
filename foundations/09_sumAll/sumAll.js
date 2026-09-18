7ae232614c34a8635dd5e129a89c7c5598fe74e1
tihonchuk.ap <tihonchuk.ap@rcit.gov-dpr.ru>
2026-09-18 15:43:56 +0300

    make ex 9 sum all

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+        let arr = [];
+        if (a !== b) {
+            console.log(`a=${a} and b=${b}`);
+            for (let i = Math.min(a, b); i < Math.max(a, b) + 1; i++) {
+                arr.push(i);
+            }
+        } else {
+            return a;
+        }
+        let sum = arr.reduce((acc, x) => acc + x, 0);
+        return sum;
+    } else return "ERROR";
 };
 
 // Do not edit below this line
