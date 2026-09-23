06e5156e169401befc81a74565679bddbbe22048
tihonchuk.ap <tihonchuk.ap@rcit.gov-dpr.ru>
2026-09-23 17:18:39 +0300

    make 11 exercise tempconversion

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,7 +2,6 @@ const sumAll = function (a, b) {
     if (a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
         let arr = [];
         if (a !== b) {
-            console.log(`a=${a} and b=${b}`);
             for (let i = Math.min(a, b); i < Math.max(a, b) + 1; i++) {
                 arr.push(i);
             }
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+    return roundNumber((temp - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+    return roundNumber(temp * (9 / 5) + 32);
+};
+
+const roundNumber = function (number) {
+    return Math.round(number * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
